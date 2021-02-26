b88465beadc3f68f38a2ca31f7370f826d40796b
kir-hub <kirill.silin.12321@gamil.com>
2021-02-26 03:40:08 +0200

    Update index3.js

--- index3.js
+++ index3.js
@@ -14,17 +14,17 @@ publish = function(array, condition){
     let chars = 0
     
     
-    for(let i = 0; i< initial.length ; i++){
+    for(let i = 0; i< array.length ; i++){
         let space = ''
         
-        for(let j = 0  ; j< initial[i].length ; j++){
+        for(let j = 0  ; j< array[i].length ; j++){
             
-            chars = chars + initial[i][j].length 
+            chars = chars + array[i][j].length 
             
             
         }
         
-        chars = chars + (initial[i].length - 1)
+        chars = chars + (array[i].length - 1)
         
         for(let i = 0; i < limit-chars; i++){
             const space2 = ' '
@@ -35,27 +35,27 @@ publish = function(array, condition){
 
         if(chars <= limit && condition[i] === "LEFT"){
 
-            result.push(`${initial[i].join(' ')+space}*`)
+            result.push(`${array[i].join(' ')+space}*`)
             
 
         }else if(chars > limit && condition[i] === "RIGHT"){
 
-            for(let j = 0; j<= initial[1].length - 3 ; j++){
+            for(let j = 0; j<= array[1].length - 3 ; j++){
                 
                 const space2 = ' '
                 space = space + space2
 
             }
-            result.push(`${space + initial[1][0] +' ' +initial[1][1] +' '+ initial[1][2]}*`)
+            result.push(`${space + array[1][0] +' ' +array[1][1] +' '+ array[1][2]}*`)
     space  = ''
-            for(let j = 4; j< initial[1].length ; j++){
+            for(let j = 4; j< array[1].length ; j++){
                 
                 
                 const space2 = ' '
                 space = space + space2
                 
             }
-            result.push(`${space + initial[1][3] +' ' + initial[1][4] +' ' + initial[1][5]}*`)
+            result.push(`${space + array[1][3] +' ' + array[1][4] +' ' + array[1][5]}*`)
             
         }
         
@@ -73,51 +73,3 @@ console.log(result);
 
 
 
-
-
-
-
-
-
-
-
-
-// format = function(array){
-//     let quantity = 0
-
-//     for(elem of array){
-//         for(let i = 0 ;i <array.length ;i++){
-            
-//         }
-
-
-//     }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let w = 0
-
-// for (let i = 0; i <= initial.length ;i++ ){
-    
-//     for(let i = 0; i <= initial.length ;i++){
-//         let q = initial[i].length
-//         w = w + q
-//     }
-
-//     w = 0
-// }
